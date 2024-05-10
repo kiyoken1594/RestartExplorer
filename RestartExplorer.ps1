930857978928f808243cea05d8cbb23c9552032f
kiyoken1594 <kiyokenchoukai1594@outlook.jp>
2024-05-10 20:00:45 +0900

    create shell script.

---
+++ RestartExplorer.ps1
@@ -0,0 +1,12 @@
+$path_arr = (New-Object -ComObject 'Shell.Application').Windows() | ForEach-Object { 
+  $localPath = $_.Document.Folder.Self.Path 
+  "C:\WINDOWS\explorer.exe `"$localPath`""
+}
+
+taskkill /F /IM explorer.exe
+
+for ($i = 0; $i -lt $path_arr.Length; $i++) {
+    Invoke-Expression $path_arr[$i]
+}
+
+explorer.exe
